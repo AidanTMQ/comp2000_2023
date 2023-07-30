2784f519dfac20d27ba4e4d68d3584db83c3fe08
pickyt <aidan.taylor1@students.mq.edu.au>
2023-07-31 09:28:53 +1000

    Add some JFrame related code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Red vs. Blue");
+        Main window = new Main();
+
+    }
+    private Main(){
+        
     }
 }
 
