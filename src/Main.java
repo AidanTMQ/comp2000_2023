8770a3e49773d9e14932402c29ebf2fdc150cfc9
pickyt <picky.games.ch@gmail.com>
2023-07-31 09:57:56 +1000

    add some extra lines to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
-import javax.swing.JFrame;
+import java.awt.Dimension;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Red vs. Blue");
-        Main window = new Main();
 
+        Dimension d1 = new Dimension(800, 600);
+
+        System.out.println(d1);
+
+        Dimension d2 = new Dimension();
+        
+        System.out.println(d2);
+
+        int x1 = 0;
+        System.out.println(x1);
     }
     private Main(){
-        
+    
     }
 }
 
