8426cd8ab8cd22b77987224472378c6cfbac1bdc
AidanTMQ <aidan.taylor1@students.mq.edu.au>
2023-08-21 17:50:49 +1000

    Add optional instead of potentially parsing through
    potentially needs error handling for the optional cell

--- src/Grid.java
+++ src/Grid.java
@@ -1,28 +1,40 @@
 // Author: Aidan Taylor
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.Optional;
 
 public class Grid {
-  // fields
-  Cell[][] cells = new Cell[20][20];
-  
-  // constructors
-  public Grid() {
-    for(int i=0; i<cells.length; i++) {
-      for(int j=0; j<cells[i].length; j++) {
-        cells[i][j] = new Cell(10+Cell.size*i, 10+Cell.size*j);
-      }
+    // fields
+    Cell[][] cells = new Cell[20][20];
+
+    // constructors
+    public Grid() {
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = new Cell(10 + Cell.size * i, 10 + Cell.size * j);
+            }
+        }
     }
-  }
-  public Cell getCell(int x, int y){
-    return cells[x][y];
-  }
-  // methods
-  public void paint(Graphics g, Point mousePos) {
-    for(int i=0; i<cells.length; i++) {
-      for(int j=0; j<cells[i].length; j++) {
-        cells[i][j].paint(g, mousePos);
-      }
+
+
+        // methods
+    public Optional<Cell> cellAtPoint(Point p){
+        Cell cell = cells[p.x][p.y];
+        Optional<Cell> res = Optional.of(cell);
+        return res;
     }
-  }
+    
+    public Optional<Cell> cellAtPoint(int x , int y){
+        Cell cell = cells[x][y];
+        Optional<Cell> res = Optional.of(cell);
+        return res;
+    }
+
+    public void paint(Graphics g, Point mousePos) {
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j].paint(g, mousePos);
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,43 +8,43 @@ import javax.swing.JPanel;
 import java.util.ArrayList;
 
 public class Main extends JFrame {
-    public static void main(String[] args) throws Exception {
-      Main window = new Main();
-      window.run();
+  public static void main(String[] args) throws Exception {
+    Main window = new Main();
+    window.run();
+  }
+
+  class Canvas extends JPanel {
+    Grid grid = new Grid();
+    ArrayList<Actor> actors = new ArrayList<Actor>();
+    Stage stage;
+
+    public Canvas() {
+      setPreferredSize(new Dimension(720, 720));
+      actors.add(new Bird(grid.cellAtPoint(0, 0).get()));
+      actors.add(new Dog(grid.cellAtPoint(1, 0).get()));
+      actors.add(new Cat(grid.cellAtPoint(2, 0).get()));
+      actors.add(new Mouse(grid.cellAtPoint(3, 0).get()));
+
+      stage = new Stage(grid, actors);
     }
 
-    class Canvas extends JPanel {
-      Grid grid = new Grid();
-      ArrayList<Actor> actors = new ArrayList<Actor>();
-      Stage stage;
-      
-      public Canvas() {
-        setPreferredSize(new Dimension(720, 720));
-        actors.add(new Bird(grid.getCell(0, 0)));
-        actors.add(new Dog(grid.getCell(1, 0)));
-        actors.add(new Cat(grid.getCell(2, 0)));
-        actors.add(new Mouse(grid.getCell(3, 0)));
-
-        stage = new Stage(grid,actors);
-      }
-
-      @Override
-      public void paint(Graphics g) {
-        stage.paint(g, getMousePosition());
-      }
+    @Override
+    public void paint(Graphics g) {
+      stage.paint(g, getMousePosition());
     }
+  }
 
-    private Main() {
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      Canvas canvas = new Canvas();
-      this.setContentPane(canvas);
-      this.pack();
-      this.setVisible(true);
-    }
+  private Main() {
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Canvas canvas = new Canvas();
+    this.setContentPane(canvas);
+    this.pack();
+    this.setVisible(true);
+  }
 
-    public void run() {
-      while(true) {
-        repaint();
-      }
+  public void run() {
+    while (true) {
+      repaint();
     }
+  }
 }
